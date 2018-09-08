c7e2e1f0dcbce9c3e98ee0c6bb8af6a8b34c34dc
Dkazem91 <Dkazemian@gmail.com>
2018-09-08 04:24:19 +0000

    updated css and jQuery on 2

--- web_dynamic/static/1-hbnb.js
+++ web_dynamic/static/1-hbnb.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     let amenList = [];
     $('input:checked').each(function() {
       amenList.push($(this).attr('data-name'));
-      console.log($(this).attr('data-name'));
     });
     $('.amenities h4').text(amenList.join(', '));
+    if (amenList.length == 0) {
+      $('.amenities h4').html('&nbsp;');
+    }
   });
 });
