5e86792d2e92419771343ea32daa9b3185693e3d
Isaac Wong <isaac@thirdcaptain.com>
2018-09-09 19:44:26 +0000

    End of mandatory commit

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  $('input[type=checkbox]').on("click", function () {
+  $('input[type=checkbox]').on('click', function () {
     let amenList = [];
-    $('input:checked').each(function() {
+    $('input:checked').each(function () {
       amenList.push($(this).attr('data-name'));
     });
     $('.amenities h4').text(amenList.join(', '));
-    if (amenList.length == 0) {
+    if (amenList.length === 0) {
       $('.amenities h4').html('&nbsp;');
     }
   });
